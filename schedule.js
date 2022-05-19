1b613b6fdca05140bd75bf9d5865a56808dd0d14
mathilde <math68n8@stud.kea.dk>
2022-05-19 16:54:56 +0200

    i guess this works schedule

--- schedule.js
+++ schedule.js
@@ -5,24 +5,48 @@
 //vis data
 //vis data efter input af dag/ scene
 
-const form = document.querySelector("#theSchedule");
-const input = form.elements.day;
 
-const daySchedule = {
+"use strict";
+
+window.addEventListener("DOMContentLoaded", start);
+
+const form = document.querySelector("#theSchedule");
+
+const input = form.elements.day;
+let inputday;
+const Act = {
   stage: "stagename",
   day: "day",
-  programme: {
     start: "",
-    end:"",
-    act:""
+    end: "",
+    artist: ""
 
-  }
-   
   
 };
 
+const allSchedule=[];
 
-async function getData() {
+
+
+const settings = {
+  filterDay: "mon",
+  filterStage:""
+  
+}
+
+
+function start() {
+  console.log("started");
+  //adds eventlistener to button
+
+  input.addEventListener("input", selectFilter);
+  //load my json
+  loadJSON();
+
+
+}
+
+async function loadJSON() {
 
 
   const endpoint = "https://valkyriefest.herokuapp.com/schedule";
@@ -47,50 +71,140 @@ function prepareData(jsondata) {
 
 
 
-const stages = Object.entries(jsondata);
+  const stages = Object.entries(jsondata);
   stages.forEach(stage => {
 
-
     const days = Object.entries(stage[1]);
-
-   
-
     days.forEach(day => {
 
+      const times = Object.entries(day[1]);
 
-      const schedule = Object.create(daySchedule);
+      times.forEach( time => {
 
-      schedule.stage = stage[0];
-      schedule.day = day[0];
-      schedule.programme.start = day[1];
+      const act = Object.create(Act);
+        
+      act.stage = stage[0];
+      act.day = day[0];
+      act.start = time[1].start;
+      act.end = time[1].end;
+      act.artist = time[1].act;
 
-      console.log(schedule);
+      //console.log(schedule);
 
+    allSchedule.push(act);
+    
+    
+      });
 
-displayData(schedule);
     });
-
-
-
   });
 
+  buildList(allSchedule);
 
 }
 
 
-function displayData(today) {
-  const inputday = input.value;
+
+
+//sets filter to value of input day field
+function selectFilter() {
+  inputday = input.value
+  const filter = inputday;
+  console.log(`user selected: ${filter}`);
+  setFilter(filter);
+}
+
+/*changes the value of filterDay in the settings, and calls buildlist*/
+function setFilter(filter) {
+  settings.filterDay = filter;
+  buildList();
+}
+
+/*returns a filtered schedule list based on day*/
+function filterList (filteredSchedule){
+
+  if (settings.filterDay === "mon") {
+    filteredSchedule = filteredSchedule.filter(isMonday);
+} else if (settings.filterDay === "tue") {
+    filteredSchedule = filteredSchedule.filter(isTuesday);
+} else if (settings.filterDay === "wed") {
+    filteredSchedule = filteredSchedule.filter(isWednesday);
+} else if (settings.filterDay === "thu") {
+    filteredSchedule = filteredSchedule.filter(isThursday);
+}else if (settings.filterDay === "fri") {
+  filteredSchedule = filteredSchedule.filter(isFriday);
+}else if (settings.filterDay === "sat") {
+  filteredSchedule = filteredSchedule.filter(isSaturday);
+}else if (settings.filterDay === "sun") {
+  filteredSchedule = filteredSchedule.filter(isSunday);
+}
+
+function isMonday(schedule) {
+    return schedule.day === "mon";
+}
+
+function isTuesday(schedule) {
+    return schedule.day === "tue";
+}
+
+function isWednesday(schedule) {
+    return schedule.day === "wed";
+}
+
+function isThursday(schedule) {
+    return schedule.day === "thu";
+
+}
+function isFriday(schedule) {
+  return schedule.day === "fri";
+
+}
+function isSaturday(schedule) {
+  return schedule.day === "sat";
+
+}function isSunday(schedule) {
+  return schedule.day === "sun";
+
+}
+
+return filteredSchedule;
+}
+
+/*builds the list based on the returned filtered schedule*/
+function buildList(){
+  const todaysSchedule = filterList(allSchedule);
+  displayList(todaysSchedule);
+  
+}
+
+/*clears the list, before calling display schedule for each act */
+function displayList(schedule){
+
+  document.querySelector("#schedulelist").innerHTML = "";
+  schedule.forEach(displaySchedule);
+  
+}
+
+function displaySchedule(act) {
+
 
   const clone = document.querySelector("template").content.cloneNode(true);
 
-if (today.day == inputday & today.stage == "Midgard"){
-  const program = JSON.stringify(today.programme)
-  clone.querySelector("[data-field=stage1").textContent = program;
+  if ( act.stage == "Midgard") {
+   
+    clone.querySelector("[data-field=stage1").textContent = act.artist;
+  }
+  if ( act.stage == "Vanaheim") {
+  
+    clone.querySelector("[data-field=stage2").textContent = act.artist;
+  }
+  if ( act.stage == "Jotunheim") {
+
+    clone.querySelector("[data-field=stage3").textContent = act.artist;
+  }
+
+  document.querySelector("#schedulelist").appendChild(clone);
+
+
 }
 
-  document.querySelector("#list tbody").appendChild(clone);
-
-
-}
-
-getData();
