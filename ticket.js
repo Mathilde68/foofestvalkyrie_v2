8c30630a30e33c1223b742cdbd7f7523d62b8808
KatrineVedel <katr499k@stud.kea.dk>
2022-05-21 14:48:06 +0200

    d

--- ticket.js
+++ ticket.js
@@ -20,19 +20,39 @@ function registerButtons(){
 
     //Here I add eventlistener for the increment and decrement buttons for ticket and tent
     
+    //regular
     document.getElementById("minus_r_t").addEventListener("click", deRT);
     document.getElementById("plus_r_t").addEventListener("click", inRT);
     document.getElementById("minus_r_two").addEventListener("click", deRTwo);
     document.getElementById("plus_r_two").addEventListener("click", inRTwo);
     document.getElementById("minus_r_four").addEventListener("click", deRFour);
     document.getElementById("plus_r_four").addEventListener("click", inRFour);
-    
+   //vip
+   document.getElementById("minus_v_t").addEventListener("click", deVT);
+   document.getElementById("plus_v_t").addEventListener("click", inVT);
+   document.getElementById("minus_v_two").addEventListener("click", deVTwo);
+   document.getElementById("plus_v_two").addEventListener("click", inVTwo);
+   document.getElementById("minus_v_four").addEventListener("click", deVFour);
+   document.getElementById("plus_v_four").addEventListener("click", inVFour);
 
-    
+}
+
+function showVipDetails() {
+    const vipDetail = document.querySelector(".vip_fillout");
+    console.log("LOL");
+    vipDetail.style.display = "block";
+    document.querySelector(".closeVip").addEventListener("click", () => vipDetail.style.display = "none");
+}
+function showRegDetails() {
+    const regDetail = document.querySelector(".regu_fillout");
+    console.log("LOL");
+
+    regDetail.style.display = "block";
+    document.querySelector(".closeRegu").addEventListener("click", () => regDetail.style.display = "none");   
 }
 
 function deRT (){
-    document.getElementById('amount_reg_ticket').stepDown().preventDefault();
+    document.getElementById('amount_reg_ticket').stepDown();
     
 
 }
@@ -60,24 +80,35 @@ function inRFour (){
 }
 
 
-
-
-
-function showVipDetails() {
-    const vipDetail = document.querySelector(".vip_fillout");
-    console.log("LOL");
-    vipDetail.style.display = "block";
-    document.querySelector(".closeVip").addEventListener("click", () => vipDetail.style.display = "none");
-}
-function showRegDetails() {
-    const regDetail = document.querySelector(".regu_fillout");
-    console.log("LOL");
-
-    regDetail.style.display = "block";
-    document.querySelector(".closeRegu").addEventListener("click", () => regDetail.style.display = "none");
-
+function deVT (){
+    document.getElementById('amount_vip_ticket').stepDown();
     
+
 }
+function inVT (){
+    document.getElementById('amount_vip_ticket').stepUp();
+
+}
+
+function deVTwo (){
+    document.getElementById('amountv_tent_two').stepDown();
+
+}
+function inVTwo (){
+    document.getElementById('amountv_tent_two').stepUp();
+
+}
+
+function deVFour (){
+    document.getElementById('amountv_tent_four').stepDown();
+
+}
+function inVFour (){
+    document.getElementById('amountv_tent_four').stepUp();
+
+}
+
+
 
 /* function increment() {
     document.getElementById('demoInput').stepUp();
