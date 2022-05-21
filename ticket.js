812a1a00e7235c119e61e5e3bbba7425a70942a5
KatrineVedel <katr499k@stud.kea.dk>
2022-05-21 14:38:23 +0200

    f

--- ticket.js
+++ ticket.js
@@ -17,7 +17,49 @@ function registerButtons(){
     //Here I add eventlistener for the tickets
     document.querySelector(".regu_ticket").addEventListener("click", showRegDetails);
     document.querySelector(".vip_ticket").addEventListener("click", showVipDetails);
+
+    //Here I add eventlistener for the increment and decrement buttons for ticket and tent
+    
+    document.getElementById("minus_r_t").addEventListener("click", deRT);
+    document.getElementById("plus_r_t").addEventListener("click", inRT);
+    document.getElementById("minus_r_two").addEventListener("click", deRTwo);
+    document.getElementById("plus_r_two").addEventListener("click", inRTwo);
+    document.getElementById("minus_r_four").addEventListener("click", deRFour);
+    document.getElementById("plus_r_four").addEventListener("click", inRFour);
+    
+
+    
 }
+
+function deRT (){
+    document.getElementById('amount_reg_ticket').stepDown().preventDefault();
+    
+
+}
+function inRT (){
+    document.getElementById('amount_reg_ticket').stepUp();
+
+}
+
+function deRTwo (){
+    document.getElementById('amount_tent_two').stepDown();
+
+}
+function inRTwo (){
+    document.getElementById('amount_tent_two').stepUp();
+
+}
+
+function deRFour (){
+    document.getElementById('amount_tent_four').stepDown();
+
+}
+function inRFour (){
+    document.getElementById('amount_tent_four').stepUp();
+
+}
+
+
 
 
 
@@ -36,4 +78,11 @@ function showRegDetails() {
 
     
 }
+
+/* function increment() {
+    document.getElementById('demoInput').stepUp();
+ }
+ function decrement() {
+    document.getElementById('demoInput').stepDown();
+ } */
 
